529a2cfdbc400ad54691973528c97e81b5339dba
alamnahid <nahidalam13765@gmail.com>
2023-12-19 22:26:02 +0600

    create a banner using swipper slider

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../Root/Root";
+import HomePage from "../Pages/HomePage/HomePage";
 
 const router = createBrowserRouter([
     {
       path: "/",
       element: <Root/>,
+      children: [
+        {
+          path: '/',
+          element: <HomePage/>
+        }
+      ]
     },
   ]);
 
